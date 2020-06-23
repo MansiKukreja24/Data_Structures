73c2735c91de4019b3749382e57f1fa26ce90732
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-06-24 00:13:43 +0530

    Employee Linked list

--- src/Lists/LinkedList/SinglyLinkedList/EmployeeLinkedList.java
+++ src/Lists/LinkedList/SinglyLinkedList/EmployeeLinkedList.java
@@ -2,13 +2,35 @@ package Lists.LinkedList.SinglyLinkedList;
 
 public class EmployeeLinkedList {
     private EmployeeNode head;
+    private int size;
+
 
     public void addToFront(Employee employee){
         EmployeeNode node = new EmployeeNode(employee);
         node.setNext(head);
         head = node;
+        size++;
 
     }
+
+    public boolean isEmpty(){
+        return head==null;
+    }
+
+    public EmployeeNode removeFromFront(){
+        if(isEmpty())
+            return null;
+        EmployeeNode removeNode = head;
+        head = head.getNext();
+        size--;
+        removeNode.setNext(null);
+        return removeNode;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
     public void printList(){
         EmployeeNode current = head;
         System.out.print("head ->");
