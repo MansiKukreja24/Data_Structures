16e17d7d6ecf02274752e1b17e91df4b67ddef3f
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-06-23 22:17:16 +0530

    Main class

---
+++ src/Lists/LinkedList/SinglyLinkedList/Main.java
@@ -0,0 +1,20 @@
+package Lists.LinkedList.SinglyLinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+
+        list.printList();
+    }
+
+}
