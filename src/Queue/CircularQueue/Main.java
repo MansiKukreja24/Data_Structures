8ab7706bed0889d40878e637b288a8d27e139823
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-06-27 00:24:10 +0530

    Main class

---
+++ src/Queue/CircularQueue/Main.java
@@ -0,0 +1,29 @@
+package Queue.CircularQueue;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeAdams = new Employee("Mike", "Adams", 223);
+        Employee billEnd = new Employee("Bill", "End", 221);
+
+        CircularQueue queue = new CircularQueue(5);
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.remove();
+        queue.add(marySmith);
+        queue.remove();
+        queue.add(mikeAdams);
+        queue.remove();
+        queue.add(billEnd);
+        queue.remove();
+        queue.add(janeJones);
+
+        queue.printQueue();
+
+
+    }
+}
